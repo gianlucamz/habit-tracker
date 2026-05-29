d1bd537ddcdf6c7f38159da5509fcb407feab003
gianlucamz <dd .>
2026-05-29 19:14:06 -0300

    chore: prepare backend for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,11 @@ import { errorMiddleware } from "./middlewares/error.middleware.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  }),
+);
 app.use(express.json());
 
 app.use("/auth", authRoutes);
